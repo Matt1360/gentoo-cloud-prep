fa63a030c31d834009aa395ebb5a9c62040ad196
Matthew Thode <mthode@mthode.org>
2015-05-17 22:56:05 -0500

    clean up and added portage snapshot fetching

--- 01-get-stage3.sh
+++ 01-get-stage3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Note that I use this script to update all my current stages, and rootfs,
 # but this repo is more specifically for Gentoo, so have some Gentoo.
@@ -6,22 +6,49 @@
 ##
 ## Vars
 ##
-MIRROR=http://mirror.reenigne.net
-OUTDIR=/var/tmp/catalyst/builds
+MIRROR="https://lug.mtu.edu"
+OUTDIR="/var/tmp/catalyst/builds"
+
+mkdir -p ${OUTDIR}
 
 ##
 ## Gentoo
 ##
 
-name=stage3-amd64-latest.tar.bz2
-stage3_file=`curl -s ${MIRROR}/gentoo/releases/amd64/autobuilds/latest-stage3-amd64.txt | awk '/stage3/ { print $1 }'`
-live_sha512=`curl -s ${MIRROR}/gentoo/releases/amd64/autobuilds/${stage3_file}.DIGESTS | awk '/SHA512 HASH/{getline;print}' | grep -iv 'contents' | awk {'print $1'}`
-our_sha512=`sha512sum /var/tmp/catalyst/builds/$name | awk {'print $1'}`
+STAGE3_NAME="stage3-amd64-latest.tar.bz2"
+STAGE3_FILE=$(curl -s "${MIRROR}/gentoo/releases/amd64/autobuilds/latest-stage3-amd64.txt" | awk '/stage3/ { print $1 }')
+LIVE_SHA512=$(curl -s "${MIRROR}/gentoo/releases/amd64/autobuilds/${STAGE3_FILE}.DIGESTS" | awk '/SHA512 HASH/{getline;print}' | grep -iv 'contents' | awk {'print $1'})
+OUR_SHA512=$(sha512sum "${OUTDIR}/${STAGE3_NAME}" | awk {'print $1'})
 
-if [ "$live_sha512" != "$our_sha512" ]
+# download latest stage3 if not the newest
+if [ "${LIVE_SHA512}" != "${OUR_SHA512}" ]
 then
-	echo "Downloading new image - $name"
-	curl -s ${MIRROR}/gentoo/releases/amd64/autobuilds/$stage3_file > ${OUTDIR}/$name
+	echo "Downloading new image - ${STAGE3_NAME}"
+	curl -s "${MIRROR}/gentoo/releases/amd64/autobuilds/${STAGE3_FILE}" > "${OUTDIR}/${STAGE3_NAME}"
 else
-	echo "$name is up to date, skipping"
+	echo "${STAGE3_NAME} is up to date, skipping"
+fi
+
+# make sure latest stage3 is actually good
+OUR_SHA512=$(sha512sum "${OUTDIR}/${STAGE3_NAME}" | awk {'print $1'})
+if [ "${OUR_SHA512}" != "${OUR_SHA512}" ]; then
+  echo 'downloaded file did not match the sha512 sum'
+  exit 1
+fi
+
+# get the latest portage
+PORTAGE_DIR="/var/tmp/catalyst/snapshots"
+PORTAGE_LIVE_MD5=$(curl -s "${MIRROR}/gentoo/snapshots/portage-latest.tar.bz2.md5sum" | awk '/portage-latest/ {print $1}')
+OUR_MD5=$(md5sum "${PORTAGE_DIR}/portage-latest.tar.bz2" | awk {'print $1'})
+if [[ "${PORTAGE_LIVE_MD5}" != "${OUR_MD5}" ]]; then
+  echo 'downloading new portage tarball'
+  curl -s "${MIRROR}/gentoo/snapshots/portage-latest.tar.bz2" > "${PORTAGE_DIR}/portage-latest.tar.bz2"
+else
+  echo 'portage tarball is up to date'
+fi
+
+OUR_MD5=$(md5sum "${PORTAGE_DIR}/portage-latest.tar.bz2" | awk {'print $1'})
+if [[ "${PORTAGE_LIVE_MD5}" != "${OUR_MD5}" ]]; then
+  echo 'downloaded file did not match the md5sum'
+  exit 1
 fi
