9504e7469039e76f73377b550665cb10fc1462d8
Matthew Thode <mthode@mthode.org>
2015-06-04 15:25:48 -0500

    final update to use gkeys to verify stages and snapshots

--- 01-get-stage3.sh
+++ 01-get-stage3.sh
@@ -20,21 +20,24 @@ STAGE3_REAL_PATH=$(curl -s "${MIRROR}/releases/amd64/autobuilds/latest-stage3-am
 STAGE3_REAL_NAME=$(echo -n "${STAGE3_REAL_PATH}" | awk -F/ '{ print $2}')
 STAGE3_URL="${MIRROR}/releases/amd64/autobuilds/current-stage3-amd64/${STAGE3_REAL_NAME}"
 
-echo "Downloading new image - ${STAGE3_NAME}"
-curl -s "${STAGE3_URL}" -o "${OUTDIR}/${STAGE3_REAL_NAME}"
 curl -s "${STAGE3_URL}.DIGESTS.asc" -o "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc"
-
-# make sure latest stage3 is actually good
 gkeys verify -F "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc"
-if [[ $? != 0 ]]; then
+STATUS=$?
+if [[ $STATUS != 0 ]]; then
   echo 'stage3 did not verify, removing badness'
   rm "${OUTDIR}/${STAGE3_REAL_NAME}"
-  rm "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS"
+  rm "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc"
   exit 1
-else
+fi
+
+SHA512=$(grep -A1 SHA512 "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc" | grep stage3 | grep -v CONTENTS | awk '{ print $1 }')
+SHA512_REAL=$(sha512sum "${OUTDIR}/${STAGE3_NAME}" | awk '{ print $1 }')
+if [[ "${SHA512}" != "${SHA512_REAL}" ]]; then
+  echo "Downloading new image - ${STAGE3_REAL_NAME}"
+  curl -s "${STAGE3_URL}" -o "${OUTDIR}/${STAGE3_REAL_NAME}"
   SHA512=$(grep -A1 SHA512 "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc" | grep stage3 | grep -v CONTENTS | awk '{ print $1 }')
   SHA512_REAL=$(sha512sum "${OUTDIR}/${STAGE3_REAL_NAME}" | awk '{ print $1 }')
-  if [[ SHA512 != SHA512_REAL ]]; then
+  if [[ "${SHA512}" != "${SHA512_REAL}" ]]; then
     echo 'shasum did not match, removing badness'
     rm "${OUTDIR}/${STAGE3_REAL_NAME}"
     rm "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc"
@@ -42,9 +45,10 @@ else
   fi
   # otherwise we cleanup and move on
   rm "${OUTDIR}/${STAGE3_NAME}"
-  rm "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS"
+  rm "${OUTDIR}/${STAGE3_REAL_NAME}.DIGESTS.asc"
   mv "${OUTDIR}/${STAGE3_REAL_NAME}" "${OUTDIR}/${STAGE3_NAME}"
 fi
+
 
 # get the latest portage
 PORTAGE_DIR="/var/tmp/catalyst/snapshots"
@@ -52,13 +56,19 @@ PORTAGE_LIVE_MD5=$(curl -s "${MIRROR}/snapshots/portage-latest.tar.bz2.md5sum" |
 OUR_MD5=$(md5sum "${PORTAGE_DIR}/portage-latest.tar.bz2" | awk {'print $1'})
 if [[ "${PORTAGE_LIVE_MD5}" != "${OUR_MD5}" ]]; then
   echo 'downloading new portage tarball'
-  curl -s "${MIRROR}/snapshots/portage-latest.tar.bz2" > "${PORTAGE_DIR}/portage-latest.tar.bz2"
+  curl -s "${MIRROR}/snapshots/portage-latest.tar.bz2" -o "${PORTAGE_DIR}/portage-latest.tar.bz2"
+  curl -s "${MIRROR}/snapshots/portage-latest.tar.bz2.gpgsig" -o "${PORTAGE_DIR}/portage-latest.tar.bz2.gpgsig"
+  gkeys verify -F "${PORTAGE_DIR}/portage-latest.tar.bz2"
+  STATUS=$?
+  if [[ $STATUS != 0 ]]; then
+    echo 'tarball did not verify, removing badness'
+    rm "${PORTAGE_DIR}/portage-latest.tar.bz2"
+    rm "${PORTAGE_DIR}/portage-latest.tar.bz2.gpgsig"
+    exit 1
+  elif [[ $STATUS == 0 ]]; then
+    echo 'tarball verified'
+    rm "${PORTAGE_DIR}/portage-latest.tar.bz2.gpgsig"
+  fi
 else
   echo 'portage tarball is up to date'
-fi
-
-OUR_MD5=$(md5sum "${PORTAGE_DIR}/portage-latest.tar.bz2" | awk {'print $1'})
-if [[ "${PORTAGE_LIVE_MD5}" != "${OUR_MD5}" ]]; then
-  echo 'downloaded file did not match the md5sum'
-  exit 1
 fi
